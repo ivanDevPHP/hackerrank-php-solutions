9d7196770d4ca1a352433cf05a98a6acaf0d2c04
Ivan Lima <100978340+ivanDevPHP@users.noreply.github.com>
2024-09-17 21:23:35 +0100

    Create Drawing Book.php

---
+++ implementation/Drawing Book.php
@@ -0,0 +1,9 @@
+function pageCount($n, $p) {
+     // Turns from the front
+    $frontTurns = intdiv($p, 2);
+    
+    // Turns from the back
+    $backTurns = intdiv($n, 2) - intdiv($p, 2);
+    
+    return min($frontTurns, $backTurns);
+}
