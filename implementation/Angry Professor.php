8ffffbf32ee1c754685a2b6cee837ec2cccc1316
Ivan Lima <100978340+ivanDevPHP@users.noreply.github.com>
2024-09-26 22:47:41 +0100

    Add "Angry Professor" Challenge

---
+++ implementation/Angry Professor.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * Complete the 'angryProfessor' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY a
+ */
+
+function angryProfessor($k, $a) {
+    // Write your code here
+    // Count how many students arrived on time
+    $onTime = 0;
+    for($i = 0; $i < count($a); $i++){
+        if($a[$i] <= 0){
+            $onTime ++;
+        }
+    }
+
+    return ($onTime >= $k) ? 'NO' : 'YES';
+}
+$fp
