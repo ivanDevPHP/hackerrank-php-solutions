7d22e97a1bf92e7e5fe1b4fe0978505acbf0786c
Ivan Lima <100978340+ivanDevPHP@users.noreply.github.com>
2024-10-16 19:59:03 +0100

    Update Halloween Sale.php

--- implementation/Halloween Sale.php
+++ implementation/Halloween Sale.php
@@ -1,3 +1,16 @@
+<?php
+
+/*
+ * Complete the 'howManyGames' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER p
+ *  2. INTEGER d
+ *  3. INTEGER m
+ *  4. INTEGER s
+ */
+
 function howManyGames($p, $d, $m, $s) {
     // Initialize the total amount of money spent on games
     $moneySpent = 0;
